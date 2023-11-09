9bdcf036c76a52bb4816f145a32a0fd9ce526168
Max Hallgarten La Casta <34109153+maxhlc@users.noreply.github.com>
2023-11-09 15:06:43 +0000

    Propagator wrapper

--- brent/propagators.py
+++ brent/propagators.py
@@ -133,3 +133,47 @@ def pv_to_array(states):
 
     # Return dates and states
     return dates_, states_
+
+
+class Propagator:
+    def __init__(self, propagator):
+        # Store propagator
+        self.propagator = propagator
+
+    def setInitialState(self, date, state, frame=DEFAULT_ECI, mu=DEFAULT_MU):
+        # Convert initial date and state to Orekit format
+        date_ = datetime_to_absolutedate(date)
+        pos = Vector3D(*state[0:3].tolist())
+        vel = Vector3D(*state[3:6].tolist())
+
+        # Generate new spacecraft state
+        state = TimeStampedPVCoordinates(date_, pos, vel)
+        orbit = CartesianOrbit(state, frame, mu)
+        spacecraftState = SpacecraftState(orbit)
+
+        # Update propagator initial state
+        self.propagator.setInitialState(spacecraftState)
+
+    def propagate(self, dates, frame=DEFAULT_ECI):
+        # Declare states array
+        states = np.empty((len(dates), 6))
+        states.fill(np.nan)
+
+        # Iterate through dates
+        for idx, date in enumerate(dates):
+            # Convert date to Orekit format
+            date_ = datetime_to_absolutedate(date)
+
+            # Propagate state
+            state = self.propagator.getPVCoordinates(date_, frame)
+
+            # Extract position and velocity
+            pos = state.getPosition().toArray()
+            vel = state.getVelocity().toArray()
+
+            # Update state matrix
+            states[idx, 0:3] = pos
+            states[idx, 3:6] = vel
+
+        # Return states array
+        return states
