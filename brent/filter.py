e2138c87f0522c6b6587cfd881369125e8126acc
Max Hallgarten La Casta <34109153+maxhlc@users.noreply.github.com>
2023-11-29 12:15:02 +0000

    Refactor for covariance provider

--- brent/filter.py
+++ brent/filter.py
@@ -4,19 +4,63 @@ import scipy.optimize
 
 # Orekit imports
 import orekit
+from orekit.pyhelpers import datetime_to_absolutedate
 from org.orekit.estimation.measurements import ObservableSatellite, PV
+from org.orekit.estimation.leastsquares import BatchLSEstimator
+from org.orekit.utils import PVCoordinates, TimeStampedPVCoordinates
 from org.hipparchus.linear import QRDecomposer
 from org.hipparchus.optim.nonlinear.vector.leastsquares import GaussNewtonOptimizer
-from org.orekit.estimation.leastsquares import BatchLSEstimator
+from org.hipparchus.geometry.euclidean.threed import Vector3D
+
+import java.util
 
 # Internal imports
+import brent.frames
 import brent.propagators
 
 
+class CovarianceProvider:
+    def __init__(self):
+        pass
+
+    def __call__(self, state):
+        # Return identity matrix
+        return np.identity(len(state))
+
+
+class RTNCovarianceProvider(CovarianceProvider):
+    def __init__(self, sigma):
+        # Store standard deviations
+        self.sigma = sigma
+
+        # Create RTN covariance matrix
+        self.covarianceRTN = np.diag(sigma) ** 2
+
+    def __call__(self, state):
+        # Calculate RTN transform
+        rtn = brent.frames.rtn(state.reshape((1, 6)))[0, :, :]
+
+        # Rotate covariance matrix to inertial frame
+        covarianceXYZ = rtn.T @ self.covarianceRTN @ rtn
+
+        # Return inertial covariance
+        return covarianceXYZ
+
+
 class OrekitBatchLeastSquares:
-    def __init__(self, states):
+    def __init__(self, dates, states, covarianceProvider=CovarianceProvider()):
+        # Generate states in Orekit format
+        states_ = [
+            TimeStampedPVCoordinates(
+                datetime_to_absolutedate(date),
+                Vector3D(*state[0:3].tolist()),
+                Vector3D(*state[3:6].tolist()),
+            )
+            for date, state in zip(dates, states)
+        ]
+
         # Create propagator builder
-        propagatorBuilder = brent.propagators.default_propagator_builder_(states[0])
+        propagatorBuilder = brent.propagators.default_propagator_builder_(states_[0])
 
         # Create decomposer and optimiser
         matrixDecomposer = QRDecomposer(1e-11)
@@ -34,14 +78,21 @@ class OrekitBatchLeastSquares:
         satellite = ObservableSatellite(0)
 
         # Create observations
-        for state in states:
+        for state, state_ in zip(states, states_):
+            # Calculate covariance matrix
+            covariance = covarianceProvider(state)
+
+            # Extract position and velocity sigmas
+            sigmaPosition = np.sqrt(np.diag(covariance[0:3, 0:3])).tolist()
+            sigmaVelocity = np.sqrt(np.diag(covariance[3:6, 3:6])).tolist()
+
             # Create observation
             pv = PV(
-                state.getDate(),
-                state.getPosition(),
-                state.getVelocity(),
-                1.0,
-                1.0,
+                state_.getDate(),
+                state_.getPosition(),
+                state_.getVelocity(),
+                sigmaPosition,
+                sigmaVelocity,
                 1.0,
                 satellite,
             )
@@ -53,8 +104,20 @@ class OrekitBatchLeastSquares:
         self.estimator = estimator
 
     def estimate(self):
-        # Execute estimator
-        return self.estimator.estimate()[0]
+        # Execute fit propagator
+        return brent.propagators.Propagator(self.estimator.estimate()[0])
+
+    def covariance(self):
+        # Extract estimated covariance
+        covariance_ = self.estimator.getPhysicalCovariances(1e-16)
+
+        # Convert to NumPy array
+        covariance = np.array(
+            [covariance_.getRow(irow) for irow in range(covariance_.getRowDimension())]
+        )
+
+        # Return fit covariance
+        return covariance
 
 
 class Weight:
