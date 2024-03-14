dd832d9c05daaa1929a99be4e1fb7d4ba43b3977
Max Hallgarten La Casta <34109153+maxhlc@users.noreply.github.com>
2024-03-14 12:19:51 +0000

    Keplerian conversions

--- brent/frames.py
+++ brent/frames.py
@@ -1,6 +1,16 @@
 # Third-party imports
 import numpy as np
 
+# Orekit imports
+import orekit
+from orekit.pyhelpers import datetime_to_absolutedate
+from org.orekit.orbits import KeplerianOrbit, PositionAngle
+from org.orekit.utils import TimeStampedPVCoordinates
+from org.hipparchus.geometry.euclidean.threed import Vector3D
+
+# Internal imports
+from brent.propagators import DEFAULT_ECI, DEFAULT_MU
+
 
 def rtn(states):
     # Extract reference position and velocity vectors
@@ -27,3 +37,65 @@ def rtn(states):
 
     # Return transformation matrix
     return RTN
+
+
+def cartesian_to_keplerian(dates, states, mu=DEFAULT_MU, frame=DEFAULT_ECI):
+    def _cartesian_to_keplerian(date, state):
+        # Convert date and state to Orekit format
+        dat = datetime_to_absolutedate(date)
+        pos = Vector3D(*state[0:3].tolist())
+        vel = Vector3D(*state[3:6].tolist())
+
+        # Create spacecraft state
+        pv = TimeStampedPVCoordinates(dat, pos, vel)
+
+        # Convert to Keplerian state
+        keplerian = KeplerianOrbit(pv, frame, mu)
+
+        # Return extracted Keplerian elements
+        return [
+            keplerian.getA(),
+            keplerian.getE(),
+            keplerian.getI(),
+            keplerian.getRightAscensionOfAscendingNode(),
+            keplerian.getPerigeeArgument(),
+            keplerian.getMeanAnomaly(),
+        ]
+
+    # Return Keplerian elements
+    return np.array(
+        [_cartesian_to_keplerian(date, state) for date, state in zip(dates, states)]
+    )
+
+
+def keplerian_to_cartesian(dates, states, mu=DEFAULT_MU, frame=DEFAULT_ECI):
+    def _keplerian_to_cartesian(date, state):
+        # Convert date to Orekit format
+        dat = datetime_to_absolutedate(date)
+
+        # Extract Keplerian elements
+        a, e, i, raan, aop, ta = state
+
+        # Ensure that the variables are floats
+        a = float(a)
+        e = float(e)
+        i = float(i)
+        raan = float(raan)
+        aop = float(aop)
+        ta = float(ta)
+
+        # Create Keplerian representation
+        kep = KeplerianOrbit(a, e, i, aop, raan, ta, PositionAngle.MEAN, frame, dat, mu)
+
+        # Extract position and velocity
+        pv = kep.getPVCoordinates()
+        pos = pv.getPosition().toArray()
+        vel = pv.getVelocity().toArray()
+
+        # Return extracted Cartesian state
+        return np.array([*pos, *vel])
+
+    # Return Cartesian states
+    return np.array(
+        [_keplerian_to_cartesian(date, state) for date, state in zip(dates, states)]
+    )
