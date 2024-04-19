9dc3e3470086f262d5e97b48d2b6b4a9d3ba4d41
Max Hallgarten La Casta <34109153+maxhlc@users.noreply.github.com>
2024-04-19 16:44:12 +0100

    Added synthetic TLE generator

--- brent/filter/__init__.py
+++ brent/filter/__init__.py
@@ -2,3 +2,4 @@
 from .batchleastsquares import BatchLeastSquares
 from .covariance import CovarianceProvider, RTNCovarianceProvider
 from .observations import generate_observations
+from .stle import SyntheticTLEGenerator
---
+++ brent/filter/stle.py
@@ -0,0 +1,167 @@
+# Standard imports
+from copy import deepcopy
+from datetime import datetime
+
+# Third-party imports
+import numpy as np
+import scipy.optimize
+
+# Orekit imports
+import orekit
+from orekit.pyhelpers import datetime_to_absolutedate
+from org.orekit.orbits import CartesianOrbit
+from org.orekit.propagation import SpacecraftState
+from org.orekit.propagation.analytical.tle import TLE
+from org.orekit.propagation.analytical.tle.generation import (
+    FixedPointTleGenerationAlgorithm,
+)
+from org.orekit.utils import TimeStampedPVCoordinates
+from org.hipparchus.geometry.euclidean.threed import Vector3D
+
+# Internal imports
+from brent import Constants
+from brent.propagators import TLEPropagator
+
+
+class SyntheticTLEGenerator:
+
+    def __init__(self, dates, states):
+        # Store copies of the dates and states
+        self.dates = deepcopy(dates)
+        self.states = deepcopy(states)
+
+        # Convert final date and state to Orekit format
+        # TODO: repeated across codebase, move to separate function
+        date_ = datetime_to_absolutedate(dates[-1])
+        pos_ = Vector3D(*states[-1, 0:3].tolist())
+        vel_ = Vector3D(*states[-1, 3:6].tolist())
+        state_ = TimeStampedPVCoordinates(date_, pos_, vel_)
+        orbit = CartesianOrbit(state_, Constants.DEFAULT_ECI, Constants.DEFAULT_MU)
+
+        # Create template TLE
+        templateTLE = SyntheticTLEGenerator.__generate_tle(date=dates[-1])
+
+        # Generate initial TLE
+        initialTLE = TLE.stateToTLE(
+            SpacecraftState(orbit),
+            templateTLE,
+            FixedPointTleGenerationAlgorithm(),
+        )
+
+        # Generate initial guess
+        self.x0 = SyntheticTLEGenerator.__tle_to_variables(initialTLE)
+
+    def estimate(self) -> TLEPropagator:
+        # Load dates and states
+        dates = self.dates
+        date = dates[-1]
+        states = self.states
+
+        # Load initial guess
+        x0 = self.x0
+
+        def fun(x):
+            # Generate TLE
+            tle = SyntheticTLEGenerator.__variables_to_tle(date, x)
+
+            # Propagate TLE
+            states_ = TLEPropagator([tle]).propagate(dates)
+
+            # Calculate state error
+            delta = states_ - states
+
+            # Return vector of errors
+            return delta.ravel()
+
+        # Execute optimiser
+        sol = scipy.optimize.least_squares(fun, x0, method="lm")
+
+        # Create TLE
+        tle = SyntheticTLEGenerator.__variables_to_tle(date, sol.x)
+
+        # Return propagator
+        return TLEPropagator([tle])
+
+    @staticmethod
+    def __generate_tle(
+        date: datetime,
+        satelliteNumber: int = 0,
+        classification: str = " ",
+        launchYear: int = 0,
+        launchNumber: int = 0,
+        launchPiece: str = "   ",
+        ephemerisType: int = 0,
+        elementNumber: int = 0,
+        revolutionNumberAtEpoch: int = 0,
+        meanMotion: float = 0.0,
+        meanMotionFirstDerivative: float = 0.0,
+        meanMotionSecondDerivative: float = 0.0,
+        e: float = 0.0,
+        i: float = 0.0,
+        aop: float = 0.0,
+        raan: float = 0.0,
+        meanAnomaly: float = 0.0,
+        bStar: float = 0.0,
+    ) -> TLE:
+        # Return TLE template
+        return TLE(
+            satelliteNumber,
+            classification,
+            launchYear,
+            launchNumber,
+            launchPiece,
+            ephemerisType,
+            elementNumber,
+            datetime_to_absolutedate(date),
+            meanMotion,
+            meanMotionFirstDerivative,
+            meanMotionSecondDerivative,
+            e,
+            i,
+            aop,
+            raan,
+            meanAnomaly,
+            revolutionNumberAtEpoch,
+            bStar,
+        )
+
+    @staticmethod
+    def __tle_to_variables(tle: TLE) -> np.ndarray:
+        # Return variables
+        return np.array(
+            [
+                tle.getMeanMotion(),
+                tle.getE(),
+                tle.getI(),
+                tle.getRaan(),
+                tle.getPerigeeArgument(),
+                tle.getMeanAnomaly(),
+                tle.getBStar(),
+            ]
+        )
+
+    @staticmethod
+    def __variables_to_tle(date: datetime, var: np.ndarray) -> TLE:
+        # Extract variables
+        n, e, i, raan, aop, ma, bstar = var
+
+        # Cast variables to correct datatype
+        n = float(n)
+        e = float(e)
+        i = float(i)
+        raan = float(raan)
+        aop = float(aop)
+        ma = float(ma)
+        bstar = float(bstar)
+
+        # Return TLE
+        return SyntheticTLEGenerator.__generate_tle(
+            date=date,
+            meanMotion=n,
+            e=e,
+            i=i,
+            aop=aop,
+            raan=raan,
+            meanAnomaly=ma,
+            bStar=bstar,
+        )
