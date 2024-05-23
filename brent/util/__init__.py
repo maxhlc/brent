5a5d774a1969085dbba7d998b67569feea4fa746
Max Hallgarten La Casta <34109153+maxhlc@users.noreply.github.com>
2024-05-23 12:27:51 +0100

    Add CDDIS downloader

--- brent/util/__init__.py
+++ brent/util/__init__.py
@@ -1,2 +1,3 @@
 # Internal imports
+from .cddis import CDDISDownloader
 from .permutations import generate_parameter_permutations
---
+++ brent/util/cddis.py
@@ -0,0 +1,42 @@
+# Standard imports
+import os
+
+# Third-party imports
+import requests
+
+
+class CDDISDownloader:
+
+    def __init__(self, username: str, password: str) -> None:
+        # Store authentication parameters
+        self.username = username
+        self.password = password
+
+        # Open session
+        self.session = requests.Session()
+
+        # Set authentication parameters
+        self.session.auth = (self.username, self.password)
+
+    def __del__(self):
+        # Close session
+        self.session.close()
+
+    def download(self, url: str, filepath: str, timeout: float = 10) -> None:
+        # Request file (repeated for authentication)
+        # Solution from: https://urs.earthdata.nasa.gov/documentation/for_users/data_access/python (accessed 2024-05-23)
+        response_ = self.session.request("get", url)
+        response = self.session.get(response_.url, timeout=timeout)
+
+        # Raise any errors
+        response.raise_for_status()
+
+        # Ensure file directory exists
+        os.makedirs(
+            os.path.dirname(os.path.abspath(filepath)),
+            exist_ok=True,
+        )
+
+        with open(filepath, "wb") as fid:
+            # Save file
+            fid.write(response.content)
