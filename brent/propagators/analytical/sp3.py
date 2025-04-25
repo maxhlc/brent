1094befad6f3b8b9e2b689e0a1819a0fa0fd2eb8
Max Hallgarten La Casta <34109153+maxhlc@users.noreply.github.com>
2025-04-25 11:41:51 +0100

    Added method to load all ephemerides from SP3 files

--- brent/propagators/analytical/sp3.py
+++ brent/propagators/analytical/sp3.py
@@ -9,7 +9,7 @@ from functools import cache
 import orekit
 from orekit.pyhelpers import absolutedate_to_datetime
 from org.orekit.data import DataSource
-from org.orekit.files.sp3 import SP3, SP3Parser
+from org.orekit.files.sp3 import SP3, SP3Parser, SP3Segment
 from org.orekit.propagation.analytical import AggregateBoundedPropagator
 import java.util
 
@@ -26,8 +26,67 @@ class SP3Propagator(WrappedPropagator):
         # Return parsed SP3 file
         return SP3Parser().parse(DataSource(path))
 
-    @staticmethod
-    def load(path: str, id: str) -> SP3Propagator:
+    @classmethod
+    def _generate(cls, sp3list: list[SP3], id: str) -> SP3Propagator:
+        # Declare list for SP3 propagators
+        sp3propagators = []
+
+        # Iterate through SP3 data
+        for sp3 in sp3list:
+            try:
+                # Extract segments
+                segments: list[SP3Segment] = list(sp3.getEphemeris(id).getSegments())
+
+                # Iterate through segments
+                for segment in segments:
+                    # Extract propagator
+                    propagator = segment.getPropagator()
+
+                    # Add to propagator list
+                    sp3propagators.append(propagator)
+
+            except orekit.JavaError:
+                # TODO: log error?
+                continue
+
+        # Sort propagators by ascending starting date
+        sp3propagators.sort(key=lambda x: absolutedate_to_datetime(x.getMinDate()))
+
+        # Declare Java list for SP3 propagators
+        sp3propagators_ = java.util.ArrayList()
+        [sp3propagators_.add(sp3propagator) for sp3propagator in sp3propagators]
+
+        # Create aggregate propagator
+        sp3aggregated = AggregateBoundedPropagator(sp3propagators_)
+
+        # Return propagator
+        return SP3Propagator(sp3aggregated)
+
+    @classmethod
+    def load(cls, path: str, id: str) -> SP3Propagator:
+        # Create glob of paths
+        paths = glob(path, recursive=True)
+
+        # Generate list of SP3s
+        sp3list: list[SP3] = []
+
+        # Iterate through paths
+        for path in paths:
+            try:
+                # Load SP3 file
+                sp3 = SP3Propagator._parse(path)
+
+                # Append to list of SP3s
+                sp3list.append(sp3)
+            except orekit.JavaError:
+                # TODO: log error?
+                pass
+
+        # Return propagator
+        return cls._generate(sp3list, id)
+
+    @classmethod
+    def load_all(cls, path: str) -> dict[str, SP3Propagator]:
         # Create glob of paths
         paths = glob(path, recursive=True)
 
@@ -46,34 +105,14 @@ class SP3Propagator(WrappedPropagator):
                 # TODO: log error?
                 pass
 
-        # Declare list for SP3 propagators
-        sp3propagators = []
+        # Extract SP3 identifiers
+        sp3headers = [sp3.getHeader() for sp3 in sp3list]
+        sp3ids = set(
+            [id for sp3header in sp3headers for id in list(sp3header.getSatIds())]
+        )
 
-        # Iterate through SP3 data
-        for sp3 in sp3list:
-            # Extract segments
-            segments = sp3.getEphemeris(id).getSegments()
+        # Generate propagators
+        propagators = {id: cls._generate(sp3list, id) for id in sp3ids}
 
-            # Extract number of segments
-            ns = segments.size()
-
-            # Iterate through segments
-            for idx in range(ns):
-                # Extract propagator
-                propagator = segments.get(idx).getPropagator()
-
-                # Add to propagator list
-                sp3propagators.append(propagator)
-
-        # Sort propagators by ascending starting date
-        sp3propagators.sort(key=lambda x: absolutedate_to_datetime(x.getMinDate()))
-
-        # Declare Java list for SP3 propagators
-        sp3propagators_ = java.util.ArrayList()
-        [sp3propagators_.add(sp3propagator) for sp3propagator in sp3propagators]
-
-        # Create aggregate propagator
-        sp3aggregated = AggregateBoundedPropagator(sp3propagators_)
-
-        # Return propagator
-        return SP3Propagator(sp3aggregated)
+        # Return propagators
+        return propagators
